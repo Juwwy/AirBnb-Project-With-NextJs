3204770646a2fd893eeed06b04e8b989f0cf8c77
Olowu Juwon <Juwonolowu52@gmail.com>
2021-09-02 17:18:21 +0100

    AirBnb Project with NextJs

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800 mt-10">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome clone</p>
+            <p>Refferrals accepted</p>
+            <p>100% Juwwy Work</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>Educative Build</p>
+            <p>Standard design</p>
+            <p>Advance acknolegdement</p>
+            <p>Join my team</p>
+            <p>Winners Dev</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>One big Fam </p>
+            
+        </div>
+        
+    </div>
+
+    )
+}
+
+export default Footer
